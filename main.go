a63bf365ded8dcc46d5797f0d51933e931c9cac1
Олег Шаршаков <NewPhoenixShoi@gmail.com>
2016-12-16 18:54:23 +0300

    Added VKGroupIDData struct Edit Configuration struct Added function loadConfiguration for Load Configuration from config.json and return Data Configuration Added function getVKGroupIDs for Download IDs array from VK.com and return Data IDs

--- main.go
+++ main.go
@@ -7,28 +7,30 @@ import (
 	"os"
 )
 
-var configuration struct {
+var api = &vk_api.Api{}
+
+type Configuration struct {
 	DBConfig    map[string]string `json:"DBConfig"`
 	CountGoroutine int `json:"CountGoroutine"`
 	Tokens []string `json:"Tokens"`
 }
 
+type VKGroupData struct {
+	Response struct {
+			 Count int   `json:"count"`
+			 Users []int `json:"users"`
+		 } `json:"response"`
+}
+
 func main() {
 
-	configFile, err := os.Open("config.json")
-	if err != nil {
-		log.Println("opening config file", err.Error())
-	}
+	log.Println(loadConfiguration())
+	log.Print(getVKGroupID("cat_programming","id_asc","0","100"))
 
-	jsonParser := json.NewDecoder(configFile)
-	if err = jsonParser.Decode(&configuration); err != nil {
-		log.Println("parsing config file", err.Error())
-	}
+}
 
-	log.Println(configuration.Tokens)
-
+func getVKGroupID(groupID,sort,offset,count string) VKGroupData{
 	// Login/pass auth
-	var api = &vk_api.Api{}
 	//api.AccessToken = ""
 
 	//	err := api.LoginAuth(
@@ -40,20 +42,38 @@ func main() {
 	//	if err != nil {
 	//		panic(err)
 	//	}
-
 	// Make query
 	params := make(map[string]string)
-	params["group_id"] = "cat_programming"
-	params["sort"] = "id_asc"
-	params["offset"] = "0"
-	params["count"] = "10"
-
+	params["group_id"] = groupID
+	params["sort"] = sort
+	params["offset"] = offset
+	params["count"] = count
 
 	strResp, err := api.Request("groups.getMembers", params)
 	if err != nil {
 		panic(err)
 	}
-	if strResp != "" {
-		log.Println(strResp)
+
+	var data VKGroupData
+	if err := json.Unmarshal([]byte(strResp),&data); err != nil {
+		log.Println("Parsing VK GetMembers error:", err.Error())
 	}
+
+	return data
+}
+
+func loadConfiguration() Configuration{
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		log.Println("Opening config file error:", err.Error())
+	}
+
+	jsonParser := json.NewDecoder(configFile)
+
+	var data Configuration
+	if err = jsonParser.Decode(&data); err != nil {
+		log.Println("Parsing config file error:", err.Error())
+	}
+
+	return data
 }
