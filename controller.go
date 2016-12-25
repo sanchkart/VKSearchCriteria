7f2be77710e701fb01ea6f21533a5920166b3256
Sanchir Kartiev <mlearningsystems@gmail.com>
2016-12-26 02:22:59 +0300

    fix controllers and delete main from data_access

--- controller.go
+++ controller.go
@@ -32,7 +32,7 @@ func MembersIntersect(w http.ResponseWriter, r *http.Request) {
 	if len(r.URL.RawQuery) > 0 {
 		var groups []string = r.URL.Query()["groups"]
 		var memberMin, err = strconv.ParseInt(r.URL.Query().Get("member_min"), 10, 32)
-		if(err != nil) {
+		if err != nil {
 			w.WriteHeader(404)
 		}
 
@@ -45,7 +45,7 @@ func MembersIntersect(w http.ResponseWriter, r *http.Request) {
 		request := &models.Request{
 			RequestUuid:	1,
 			UserUuid:	1,
-			TypeRequest:	"Merge",
+			TypeRequest:	"Create",
 			CreatedAt: time.Now(),
 		}
 
@@ -57,18 +57,18 @@ func MembersIntersect(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetResult(w http.ResponseWriter, r *http.Request) {
-	db := pg.Connect(&pg.Options{
-		User: "postgres",
-		Password: "411207",
-	})
-	data_access.CreateSchema(db)
-	result := &models.Result{
-		RequestUuid:	1,
-		Id:	1,
-		ResultId:	1,
-		AddedAt: time.Now(),
+	if len(r.URL.RawQuery) > 0 {
+		var id, err = strconv.ParseInt(r.URL.Query().Get("request_id"), 10, 32)
+		if err != nil {
+			w.WriteHeader(404)
+		}
+
+		db := pg.Connect(&pg.Options{
+			User:     "postgres",
+			Password: "411207",
+		})
+
+		var data= data_access.ReadResult(db, id);
+		fmt.Fprintln(w, data.Id)
 	}
-	data_access.InsertResult(db, result)
-	var data = data_access.ReadResult(db, 1);
-	fmt.Fprintln(w, data.Id)
 }
